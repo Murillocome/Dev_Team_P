beaf129bfa2b074f48b68f92f21483c138e401c4
FernandoJ07 <fernandogerdez@gmail.com>
2024-09-30 00:08:23 -0500

    Ruta save document en routers

--- app/api/routers.py
+++ app/api/routers.py
@@ -1,5 +1,5 @@
 import pydantic
-from fastapi import APIRouter, Depends
+from fastapi import APIRouter, Depends, UploadFile, File
 from pydantic import BaseModel
 from app import usecases
 from app.api import dependencies
@@ -13,6 +13,12 @@ def sing_up(username: str, password: str,
     rag_service.sing_up(username, password)
     return {"status": "User created successfully"}
 
+@rag_router.post("/save-document/", status_code=201)
+def save_document(file: UploadFile = File(...),
+                        rag_service: usecases.RAGService = Depends(dependencies.RAGServiceSingleton.get_instance)):
+    # Guardar la información del archivo en MongoDB
+    rag_service.save_document(file)
+    return {"status": "Document saved successfully"}
 
 @rag_router.post("/generate-answer/", status_code=200)
 def generate_answer(query: str,
@@ -20,8 +26,3 @@ def generate_answer(query: str,
     return {"answer": rag_service.generate_answer(query)}
 
 
-@rag_router.post("/save-document/", status_code=201)
-def save_document(document: DocumentInput,
-                  rag_service: usecases.RAGService = Depends(dependencies.RAGServiceSingleton.get_instance)):
-    rag_service.save_document(content=document.content)
-    return {"status": "Document saved successfully"}
